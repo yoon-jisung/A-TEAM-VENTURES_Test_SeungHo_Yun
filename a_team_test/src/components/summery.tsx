2b566ab06074f9429cf3fb4efaaa71cae42a1e69
yoon-jisung <hmsj2380@gmail.com>
2021-07-03 17:37:31 +0900

    summery 완성

---
+++ a_team_test/src/components/summery.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Summery() {
+  return (
+    <div className="summery">
+      <h3>들어온 요청</h3>
+      <div>파트너님에게 딱 맞는 요청서를 찾아보세요.</div>
+    </div>
+  );
+}
+
+export default Summery;
